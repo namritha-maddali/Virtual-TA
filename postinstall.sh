ae18952ffd13f81192ded38f9bf1ce1b726a809e
namritha-maddali <namritha.maddali@gmail.com>
2025-06-14 23:46:34 +0530

    railway backend

---
+++ postinstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This installs libvips so pyvips works
+apt-get update && apt-get install -y libvips
--- render_build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-apt-get update && apt-get install -y libvips # for pyvips
-pip install -r requirements.txt
